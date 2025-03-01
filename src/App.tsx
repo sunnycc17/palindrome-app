460bda17144f4628a53a964fdc9faa7f983bde0b
sunny <alkristy7@gmail.com>
2025-03-01 12:42:19 +0200

    initialized react ts bootstrap project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hello, Bootstrap + React + TypeScript!</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
